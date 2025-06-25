07061010825636f05683ecf00c5fef63bb7c1f47
Thejaswini Velvaluri <thejuvelvaluri@gmail.com>
2025-06-25 14:31:13 +0530

    Fixed image path and added my_photo.jpg

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,7 +5,11 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="image-container">
-        <img src="/my_photo.jpg" alt="THEJASWINI" className="profile-image" />
+        <img
+          src={`${process.env.PUBLIC_URL}/my_photo.jpg`}
+          alt="THEJASWINI"
+          className="profile-image"
+        />
       </div>
       <h2>Hi, I'm THEJASWINI!</h2>
       <p>I'm passionate about web development and programming.</p>
